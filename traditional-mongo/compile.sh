d5e41afdcd170be56335459cf3311755a3204c13
Josh Long <josh@joshlong.com>
2020-06-10 21:14:17 -0700

    mongodb

--- traditional-mongo/compile.sh
+++ traditional-mongo/compile.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-mvn -DskipTests=true clean package && ../compile.sh traditional com.example.traditional.TraditionalApplication 0.0.1-SNAPSHOT
+mvn -DskipTests=true clean package && ../compile.sh traditional-mongo com.example.traditional.TraditionalMongoApplication 0.0.1-SNAPSHOT
